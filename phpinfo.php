eafbf261c97657e69a577f505519bb292f5c2c57
Ludwing <ludwinglaguna@gmail.com>
2018-01-23 23:59:15 -0500

    Add files via upload

---
+++ phpinfo.php
@@ -0,0 +1,22 @@
+<?php 
+//--------------------------------------------------------------------------------------
+//-- INFORMACION TECNICA 
+//--------------------------------------------------------------------------------------
+//Localidad .............: ALCALDIA DEL MUNICIPIO GUAICAIPURO
+//Grupo Funcional........: Departamento de Informatica
+//Grupo Tecnico..........: Gerencia Funcional de AIT - PDVSA Intevep 
+//Bases de desarrollo....: Apache HTTPD Web Server 2.2.3, PHP Ver. 5.2.3, PostgreSQL 8.3
+//Periodo................: 2010
+//---------------------------------------------------------------------------------------
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>phpinfo</title>
+</head>
+
+<body>
+<?php phpinfo(); ?> 
+</body>
+</html>
